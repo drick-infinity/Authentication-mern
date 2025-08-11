73c8616555c3c44dd2ef6a801415db710ce532e2
drick-infinity <harshimalviya47487@gmail.com>
2025-08-11 03:47:42 -0700

    modified

--- server/server.js
+++ server/server.js
@@ -10,7 +10,12 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = [
+  'http://localhost:5173',  // For local development
+  'https://authentication-mern-m7i6.vercel.app',  // For production frontend
+  // Add other domains if necessary
+];
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin:allowedOrigins,credentials:true}));//save the cookeie
